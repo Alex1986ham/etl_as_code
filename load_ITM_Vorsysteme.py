0b96ad345a853295064206580ab708d4d525174c
Dudko <ALDUDKO@baumarktdirekt.local>
2019-12-12 11:01:56 +0100

    .

--- load_ITM_Vorsysteme.py
+++ load_ITM_Vorsysteme.py
@@ -86,8 +86,8 @@ dag = DAG(
         'Load_ITM_Vorsysteme',
         #catchup=False, 
         #default_args=default_args,
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
-        schedule_interval='55 * * * *',
+        start_date=datetime.datetime.now() - datetime.timedelta(hour=1),
+        schedule_interval='4 * * * *',
 )
 
 """
